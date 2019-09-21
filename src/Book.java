dc2d4e1fef5a44ad5338a7c29276c80c99929691
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2019-09-21 21:41:53 +0530

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,19 @@
+public class Book {
+    private String bookName;
+    private String authorName;
+    private String isbn;
+
+    @Override
+    public String toString() {
+        return "-----------------------------\n" + "Book Name:\t" + bookName + "\nAuthor Name:\t" + authorName + "\nISBN:\t" + isbn +
+                "\n-----------------------------";
+    }
+
+    Book(String bookName, String authorName, String isbn) {
+        this.authorName = authorName;
+        this.bookName = bookName;
+        this.isbn = isbn;
+    }
+}
+
+
---
+++ src/Store.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+    public class Store {
+        public static void main(String[]args){
+            Scanner scan = new Scanner(System.in);
+            String bookName;
+            String authorName;
+            String isbn;
+            int n = scan.nextInt();
+            scan.nextLine().trim();
+            Book[] book = new Book[n];
+            if (n == 0) {
+                System.out.println("N/A");
+            } else {
+                for (int i = 0; i < n; i++) {
+                    bookName = scan.nextLine();
+                    authorName = scan.nextLine();
+                    isbn = scan.nextLine();
+                    book[i] = new Book(bookName, authorName, isbn);
+
+                }
+                for (int i = 0; i < n; i++) {
+                    System.out.println(book[i]);
+                }
+            }
+            scan.close();
+
+        }
+    }
+
+
